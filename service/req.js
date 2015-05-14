c9f58e13eb16e812657f544b1d15bd74a01d957f
Ben Brunton <benbrunton@hotmail.com>
2015-05-14 20:32:18 +0100

    raise request timeout

--- service/req.js
+++ service/req.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 // returns a list of scraped urls
 module.exports = function(url, domain){
     var deferred = Q.defer();
-    request.get({url:url, timeout:500}, function (error, response, body) {
+    request.get({url:url, timeout:3000}, function (error, response, body) {
         
         var urls = [];
         if (!error && response.statusCode == 200 && response.headers['content-type'].indexOf('text/html') > -1) {
@@ -13,7 +13,7 @@ module.exports = function(url, domain){
             
             $('a').each(function(el){
                 var href = $(this).attr('href');
-                if(href && href.indexOf(domain) > -1 && !/^mailto/.test(href)){
+                if(href && href.indexOf(domain) > -1){
                     href = href.split('#');
                     urls.push(href[0]);
                 }
