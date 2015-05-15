c5c1e6fff0b5fafaffdb2d5d578261e937141318
Ben Brunton <benbrunton@hotmail.com>
2015-05-15 16:14:56 +0100

    link with services.slabs.io

--- settings/app.js
+++ settings/app.js
@@ -1,7 +1,12 @@
-var trawlerProcess = require('../process/app.js');
+var request = require('request');
 
+// check settings match
 exports.get = function(req, res){
-    
+    request({
+        method: 'GET',
+        url: 'http://services.slabs.io/slab/trawler/' 
+            + req.query.networkId + '/' + req.query.slabConfigId
+    }).pipe(res);
 };
 
 
@@ -10,10 +15,25 @@ exports.post = function(req, res){
     var slabConfigId = req.body.slabConfigId;
     var networkId = req.body.networkId;
     var url = req.body.url;
+    var domain = getDomain(url);
     
-    trawlerProcess.start({
-        slabConfigId: slabConfigId,
-        networkId: networkId,
-        url: url
-    });
+    request({
+        method: 'POST',
+        url:'http://services.slabs.io/slab/trawler',
+        body:{
+            slabId: slabConfigId,
+            networkId: networkId,
+            settings: {
+                url: url,
+                domain: domain
+            }
+        },
+        json:true
+    }).pipe(res);
 };
+
+function getDomain(url){
+    var removeProto = url.split('//')[1];
+    var removePath = removeProto.split('/')[0];
+    return removePath;
+}
